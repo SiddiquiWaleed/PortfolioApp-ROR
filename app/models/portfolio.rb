0026bb596bc91ede17c56db0b8b1c4b105c0b355
waleed <waleedsiddiqui44@gmail.com>
2022-08-16 02:13:55 +0500

    Added concern for placeholder images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,6 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+	include Placeholder
 
 	def self.angular
 		where(subtitle: 'Angular')
@@ -8,5 +9,12 @@ class Portfolio < ApplicationRecord
 	scope :rails_data, -> {
 		where(subtitle: 'Ruby on Rails')
 	}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+	end
 
 end
